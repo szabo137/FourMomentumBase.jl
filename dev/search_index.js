95b4dc445ea5012eeebf1364c01cb87522efdccb
Documenter.jl <documenter@juliadocs.github.io>
2024-01-26 10:16:55 +0000

    build based on fd5160a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FourMomentumBase","category":"page"},{"location":"#FourMomentumBase","page":"Home","title":"FourMomentumBase","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FourMomentumBase.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FourMomentumBase]","category":"page"}]
+}
