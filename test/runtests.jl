66afdaff63e621760f44c5b76ca7b2874edcd931
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2024-01-26 10:54:33 +0100

    added package template

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,9 @@ using Aqua
 using JET
 
 @testset "FourMomentumBase.jl" begin
-    @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(FourMomentumBase)
-    end
+#   @testset "Code quality (Aqua.jl)" begin
+#        Aqua.test_all(FourMomentumBase)
+#    end
     @testset "Code linting (JET.jl)" begin
         JET.test_package(FourMomentumBase; target_defined_modules = true)
     end
