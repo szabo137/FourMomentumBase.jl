59b07d2d51078d80c87fba8df92c2c32d93a8924
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2024-01-26 11:10:20 +0100

    removed JET

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,6 @@
 using FourMomentumBase
 using Test
-using JET
 
 @testset "FourMomentumBase.jl" begin
-    @testset "Code linting (JET.jl)" begin
-        JET.test_package(FourMomentumBase; target_defined_modules=true)
-    end
     # Write your tests here.
 end
