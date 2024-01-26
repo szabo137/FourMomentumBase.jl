fca6d86ae6460861393a13f3e40b93ae966b7583
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2024-01-26 11:07:39 +0100

    removed Aqua

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,8 @@
 using FourMomentumBase
 using Test
-using Aqua
 using JET
 
 @testset "FourMomentumBase.jl" begin
-    #   @testset "Code quality (Aqua.jl)" begin
-    #        Aqua.test_all(FourMomentumBase)
-    #    end
     @testset "Code linting (JET.jl)" begin
         JET.test_package(FourMomentumBase; target_defined_modules=true)
     end
