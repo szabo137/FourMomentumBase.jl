e94bbdf50e15dab19eb56d1db430d907a4b01e6f
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2024-05-23 23:12:20 +0200

    implemented full components for exyz

--- src/FourMomentumBase.jl
+++ src/FourMomentumBase.jl
@@ -1,10 +1,8 @@
 module FourMomentumBase
 
 export coordinate_system, coordinate_names
-export pseudorapidity, eta, rapidity, phi, energy, mass, px, py, pz
 
 include("interface.jl")
-include("coordinate_systems/xyze.jl")
-include("coordinate_systems/pt_eta_phi_m.jl")
+include("coordinate_systems/exyz.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,12 @@ using Test
 using SafeTestsets
 
 begin
+    @time @safetestset "interface" begin
+        include("interface.jl")
+    end
+
     @time @safetestset "xyze" begin
         include("xyze.jl")
     end
 
-    @time @safetestset "pt_eta_phi_m" begin
-        include("pt_eta_phi_m.jl")
-    end
 end
